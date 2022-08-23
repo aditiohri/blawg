002db3507e205ac2f1312694d04d7c0553665379
Aditi Ohri <aohri@redhat.com>
2022-08-22 22:37:46 -0500

    update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
-import astroRemark from '@astrojs/markdown-remark';
+import mdx from '@astrojs/mdx';
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
+// TO DO START HERE
+/** The RSS helper has been removed from getStaticPaths! Try the new @astrojs/rss package instead. See https://docs.astro.build/en/guides/rss/ */
 // https://astro.build/config
 export default defineConfig(
 	/** @type {import('astro').AstroUserConfig} */
 	{
 		markdown: {
-			render: [astroRemark, {
-				remarkPlugins: [],
-				rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
-					behavior: 'wrap'
-				}]],
+			render: [{
 				shikiConfig: {
 					theme: 'poimandres',
 					langs: [],
@@ -20,5 +17,10 @@ export default defineConfig(
 			}]
 		},
 		site: 'http://localhost:3000/',
-		integrations: [sitemap()]
+		integrations: [mdx({
+			remarkPlugins: [],
+			rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
+				behavior: 'wrap'
+			}]],
+		}), sitemap()]
 	});
